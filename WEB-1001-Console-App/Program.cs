ba4b7a0cb8fd4e49f3e22f39d6f41a58cf79bd8d
Brent Ritchie <ritchie.brent@gmail.com>
2021-05-27 20:07:18 -0400

    Add project files.

---
+++ WEB-1001-Console-App/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace WEB_1001_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //String s1 = new String("a");
+            //String s2 = s1.Insert(0, "b");
+            //Console.WriteLine("{0} = {1}", s1, s2);
+
+            //string[] s3 = new string[1] { "a" };
+            //string[] s4 = new string[2] { "b", "a" };
+
+            //int i1 = 4;
+            //i1 = i1 + 1;
+            //i1 += 1;
+            //i1++;
+            //++i1;
+
+            //int i4 = 34;
+            //int i2 = 0b0000000000000000000000000000000;
+            //int i3 = (int)(0b01111111111111111111111111111111);
+
+            //Console.WriteLine(i3);
+            //i3 += 1;
+            //Console.WriteLine(i3);
+
+            //Console.WriteLine(i2);
+            //i2 -= 1;
+            //Console.WriteLine(i2);
+
+
+            //UInt32 ui1 = 0b11111111111111111111111111111111;
+            //Console.WriteLine(ui1);
+            //ui1 += 1;
+            //Console.WriteLine(ui1);
+
+            //float fl1 = 0.01f;
+
+            //Console.WriteLine(0.009 + 0.001);
+            //Console.WriteLine(0.01);
+
+            // 0b000111111100000000000000000000 + 0b000000000100000000000000000000
+            // 0.1 0.101 0.103 0.11 0.12 0.13  0.2
+
+            //double d1 = 0.001d;
+
+            //decimal dec1 = 0.01M;
+            //Console.WriteLine(sizeof(decimal));
+
+            //Console.WriteLine(sizeof(bool));
+            //bool b1 = true;
+            //bool b2 = false;
+            //bool b3 = 0>1;
+
+            //Console.WriteLine(b3);
+
+            //char c1 = 'A';
+            //Console.WriteLine((byte)c1);
+
+            //byte by1 = 65;
+            //Console.WriteLine((byte)250);
+
+            //bool b1 = true;
+
+            //if (true ^ true)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            // XOR (^)
+            // 1 1 0
+            // 1 0 1
+            // 0 1 1
+            // 0 0 0
+
+            //char ele = 'c';
+
+            //switch (ele)
+            //{
+            //    case 'a':
+            //        Console.WriteLine('a');
+            //        break;
+            //    case 'b':
+            //        Console.WriteLine('b');
+            //        break;
+            //    case 'c':
+            //    case 'd':
+            //    case 'e':
+            //    default:
+            //        Console.WriteLine("other");
+            //        break;
+            //}
+
+            // int i = 5;
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //} while (i > 6);
+
+            //while (i > 6)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //int[] li = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            //for (int i = 0; i < li.Length; i++)
+            //{
+            //    int x = li[i];
+            //    Console.WriteLine(i);
+            //}
+
+            //foreach (int x in li)
+            //{
+            //    Console.WriteLine(x);
+            //}
+        }
+    }
+}
